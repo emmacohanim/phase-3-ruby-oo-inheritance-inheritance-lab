56335f66be5309479a5955a1f8441357041602b4
Emma Cohanim <emmacohanim@gmail.com>
2022-09-06 10:06:52 -0700

    commit tree 79f2e5066a90bb9758a2fefaa0c64146fd13512f

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,11 @@
-class Teacher
+require_relative './user'
+class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+  # class constraint: class variable that will not change
 
+  def teach
+    KNOWLEDGE.sample
+  end
   
 end
